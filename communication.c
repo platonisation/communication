351dc3886b7dced311e05719c3e88af9417c66cb
tbn <tbn@T-BONNIN>
2014-04-25 15:46:45 +0200

    verify if data sent is valid in readLine

--- communication.c
+++ communication.c
@@ -41,68 +41,82 @@ ssize_t Writeline(int sockd, const void *vptr, ssize_t n) {
 int Readline(int sockd, void *vptr, ssize_t maxlen) {
 
 //	//data to read
-//	unsigned char start;
-//	unsigned char src[4];
-//	unsigned char dst[4];
-//	unsigned char size;
-//	unsigned char data[20];
+	unsigned char start;
+	unsigned char src[4];
+	unsigned char dst[4];
+	unsigned char size;
+	unsigned char data[20];
+
+	read(sockd, &start, 1);
+	if (start == 0xFE) {
+		read(sockd,&size,1);
+		//size in bytes
+		if(size <= 0){//10Mo, msg
+			vptr+=2;
+		}
+		else { // files
+			//attention bug ! client envoie coucou trouve un fichier
+			debugTrace("Cannot read the message size.\n")
+			return -1;
+		}
+	}
+	else {
+		printf("Not a starting sequence\n");
+		return 0;
+	}
+	return size;
+
+//	unsigned char start = ctx->messageToReceive[0];
+	//	unsigned char size = ctx->messageToReceive[1];
+	//
+	//	if (start == 0xFE) {
+	//	//			read(sockd,src,4);
+	//	//			read(sockd,dst,4);
+	//	//size in bytes
+	//		if(size < 10000000){//10Mo, msg
+	//			debugTrace("You got a message\n");
+	//			printf("%s\n",ctx->messageToReceive);
+	//			doAction((unsigned char*)ctx->messageToReceive+2,&(ctx->messageToSend));
+	//		}
+	//		else { // files
+	//			//attention bug ! client envoie coucou trouve un fichier
+	//			debugTrace("You got a file\n");
+	//		}
+	//	}
+	//	else {
+	//		debugTrace("This is not a valid sequence\n");
+	//	}
+	//	return 1;
+
+
 //
-//	//treat data
-//	read(sockd, &start, 1);
-//	if (start == 0xFE) {
-////			read(sockd,src,4);
-////			read(sockd,dst,4);
-//		read(sockd,&size,1);
-//		printf("size:%d\n",size);
-//		//size in bytes
-//		if(size < 10000000){//10Mo, msg
-//			if((read(sockd,buffer,size) != size)){
-//				//Lecture ok
-//				printf("Cannot read %d datas\n",size);
+//	 ssize_t n, rc;
+//	    char    c, *buffer;
+//
+//	    buffer = vptr;
+//
+//	    for ( n = 1; n < maxlen; n++ ) {
+//
+//			if ( (rc = read(sockd, &c, 1)) == 1 ) {
+//				*buffer++ = c;
+//				if ( c == '\0' )
+//				break;
+//			}
+//			else if ( rc == 0 ) {
+//				if ( n == 1 )
+//				return 0;
+//				else
+//				break;
 //			}
 //			else {
-//				printf("%s",buffer);
-////				doAction(buffer,size);
+//				if ( errno == EINTR )
+//				continue;
+//				return -1;
 //			}
+//	    }
 //
-//		}
-//		else { // files
-//			//attention bug ! client envoie coucou trouve un fichier
-//			printf("This is a file\n");
-//		}
-//	}
-//	else {
-//		printf("Not a starting sequence\n");
-//	}
-//	return 1;
-
-	 ssize_t n, rc;
-	    char    c, *buffer;
-
-	    buffer = vptr;
-
-	    for ( n = 1; n < maxlen; n++ ) {
-
-			if ( (rc = read(sockd, &c, 1)) == 1 ) {
-				*buffer++ = c;
-				if ( c == '\0' )
-				break;
-			}
-			else if ( rc == 0 ) {
-				if ( n == 1 )
-				return 0;
-				else
-				break;
-			}
-			else {
-				if ( errno == EINTR )
-				continue;
-				return -1;
-			}
-	    }
-
-	    *buffer = 0;
-	    return n;
+//	    *buffer = 0;
+//	    return n;
 
 
 }
