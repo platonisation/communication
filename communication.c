fca73b83cd0660dfafbb30dafd9999175e771f6b
Platonisation <timote.bonnin@gmail.com>
2014-06-19 16:54:47 +0200

    rata

--- communication.c
+++ communication.c
@@ -7,9 +7,6 @@
 
 #include "communication.h"
 
-/*Create unparsed method*/
-/*get read line standard*/
-
 
 /*  Write a line to a socket  */
 
@@ -39,57 +36,6 @@ ssize_t Writeline(int sockd, const void *vptr, ssize_t n) {
 /*  Read a line from a socket  */
 
 int Readline(int sockd, void *vptr, ssize_t maxlen) {
-
-//	//data to read
-	unsigned char start;
-	unsigned char src[4];
-	unsigned char dst[4];
-	unsigned char size;
-	unsigned char data[20];
-
-//	read(sockd, &start, 1);
-//	if (start == 0xFE) {
-//		read(sockd,&size,1);
-//		//size in bytes
-//		if(size > 0){//10Mo, msg
-//			read(sockd, vptr, size);
-//			fclean(sockd);
-//		}
-//		else { // files
-//			//attention bug ! client envoie coucou trouve un fichier
-//			debugTrace("Cannot read the message size or = to 0.\n")
-//			return -1;
-//		}
-//	}
-//	else {
-//		printf("Not a starting sequence\n");
-//		return 0;
-//	}
-//	return size;
-
-//	unsigned char start = ctx->messageToReceive[0];
-	//	unsigned char size = ctx->messageToReceive[1];
-	//
-	//	if (start == 0xFE) {
-	//	//			read(sockd,src,4);
-	//	//			read(sockd,dst,4);
-	//	//size in bytes
-	//		if(size < 10000000){//10Mo, msg
-	//			debugTrace("You got a message\n");
-	//			printf("%s\n",ctx->messageToReceive);
-	//			doAction((unsigned char*)ctx->messageToReceive+2,&(ctx->messageToSend));
-	//		}
-	//		else { // files
-	//			//attention bug ! client envoie coucou trouve un fichier
-	//			debugTrace("You got a file\n");
-	//		}
-	//	}
-	//	else {
-	//		debugTrace("This is not a valid sequence\n");
-	//	}
-	//	return 1;
-
-
 
 	 ssize_t n, rc;
 	    char    c, *buffer;
@@ -127,12 +73,8 @@ char* parseMessage(char* buffer, int size) {
 	char* s;
 	s = malloc(size+1*sizeof(char));
 	memset(s,0,size+1);
-//	s[0]=0xFE;
-//	s[0]=strlen(buffer);
 	strcat(s,buffer);
 	s[size] = '\0';
-//	free(buffer);
-
 	return s;
 }
 
